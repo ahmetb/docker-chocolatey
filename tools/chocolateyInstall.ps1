a734320e47afec5ece48a4619df1e52bd58e62e9
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2015-03-18 23:05:42 +0000

    remove try/catch, remove Install-ChocolateyPath as it's redundant
    Signed-off-by: Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,21 +3,12 @@ $url            = 'https://master.dockerproject.com/windows/386/docker-1.5.0-dev
 $url64          = 'https://master.dockerproject.com/windows/amd64/docker-1.5.0-dev.exe'
 $validExitCodes = @(0)
 
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$packageDir = "$(Split-Path -parent $toolsDir)"
-$installDir = Join-Path "$packageDir" "bin"
-$installBin = "${packageName}.exe"
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageDir  = "$(Split-Path -parent $toolsDir)"
+$installDir  = Join-Path "$packageDir" "bin"
+$installBin  = "${packageName}.exe"
 $installPath = Join-Path "$installDir" "$installBin"
 
-try {
-  # Download binary
-  mkdir "$installDir"
-  Get-ChocolateyWebFile "$packageName" "$installPath" "$url" "$url64"
-  # Add to PATH
-  Install-ChocolateyPath "$installDir" Machine  
-
-  Write-ChocolateySuccess "$packageName"
-} catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw
-}
+mkdir "$installDir"
+Get-ChocolateyWebFile "$packageName" "$installPath" "$url" "$url64"
+Write-ChocolateySuccess "$packageName"
