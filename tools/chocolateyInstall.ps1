e28466342b2ced0a4c4970e57ca66672f713c1bb
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2015-03-24 04:36:26 +0000

    lowercase checksum

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $url            = 'http://test.docker.com.s3.amazonaws.com/builds/Windows/i386/d
 $checksum       = 'cbe2b574dd8d96825185c22fcfb280db'
 $checksumType   = 'md5'
 $url64          = 'http://test.docker.com.s3.amazonaws.com/builds/Windows/x86_64/docker-1.6.0-rc1.exe'
-$checksum64     = '00F022AD868639BB0E5FE58F360C9522'
+$checksum64     = '00f022ad868639bb0e5fe58f360c9522'
 $checksumType64 = 'md5'
 $validExitCodes = @(0)
 
