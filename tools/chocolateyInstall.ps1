ca0d9c5a4428567c2770bb16bde0eee709560caf
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2015-04-02 16:31:48 -0700

    bump to 1.6.0-rc3

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName    = 'docker'
-$url            = 'http://test.docker.com.s3.amazonaws.com/builds/Windows/i386/docker-1.6.0-rc2.exe'
-$checksum       = 'b87e30265310a9221ed4f4926ae3d579'
+$url            = 'http://test.docker.com.s3.amazonaws.com/builds/Windows/i386/docker-1.6.0-rc3.exe'
+$checksum       = '70842aaed562df66ca17654e46db2051'
 $checksumType   = 'md5'
-$url64          = 'http://test.docker.com.s3.amazonaws.com/builds/Windows/x86_64/docker-1.6.0-rc2.exe'
-$checksum64     = '32524cf8ff385ae93356c7bc8f321393'
+$url64          = 'http://test.docker.com.s3.amazonaws.com/builds/Windows/x86_64/docker-1.6.0-rc3.exe'
+$checksum64     = '7f029e2ea4eb4bc23b90e0ea0dc49836'
 $checksumType64 = 'md5'
 $validExitCodes = @(0)
 
