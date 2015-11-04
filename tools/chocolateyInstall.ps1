0cba83f765a9070273288685fefd4d211ab6ea53
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2015-11-04 13:42:06 -0800

    Update to docker 1.9.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName    = 'docker'
-$url            = 'https://get.docker.com/builds/Windows/i386/docker-1.8.1.exe'
-$checksum       = '5665ee207da5fc043f7875952f2fa5a8'
-$url64          = 'https://get.docker.com/builds/Windows/x86_64/docker-1.8.1.exe'
-$checksum64     = 'd77766fd438dcd45096a5ada2af65340'
+$url            = 'https://get.docker.com/builds/Windows/i386/docker-1.9.0.exe'
+$checksum       = '5c1990a4ce59c42ef493db0013aaccf8'
+$url64          = 'https://get.docker.com/builds/Windows/x86_64/docker-1.9.0.exe'
+$checksum64     = 'b08166276646433dc04b23982a9071aa'
 $checksumType   = 'md5'
 $checksumType64 = 'md5'
 $validExitCodes = @(0)
