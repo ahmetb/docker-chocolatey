da4d8d2ef271452f21ee9b468cf054e9ba59cc64
Stefan Scherer <scherer_stefan@icloud.com>
2016-04-09 09:54:43 +0200

    Build package for Docker 1.11.0-rc4
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,17 +1,13 @@
 $packageName    = 'docker'
-$url            = 'https://get.docker.com/builds/Windows/i386/docker-1.10.3.exe'
-$checksum       = '943235a4e0f3b1b8a214cca1d81e4693'
-$url64          = 'https://get.docker.com/builds/Windows/x86_64/docker-1.10.3.exe'
-$checksum64     = '4d9c818967d729f38931edbe33c28398'
+$url            = 'https://test.docker.com/builds/Windows/i386/docker-1.11.0-rc4.zip'
+$checksum       = '0a5ad21e3c8e08d11df1301ff9aef409'
+$url64          = 'https://test.docker.com/builds/Windows/x86_64/docker-1.11.0-rc4.zip'
+$checksum64     = 'b85671b18c24abc83ee80e3db2352363'
 $checksumType   = 'md5'
 $checksumType64 = 'md5'
 $validExitCodes = @(0)
 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$packageDir  = "$(Split-Path -parent $toolsDir)"
-$installDir  = Join-Path "$packageDir" "bin"
-$installBin  = "${packageName}.exe"
-$installPath = Join-Path "$installDir" "$installBin"
 
-New-Item -ItemType Directory -Force -Path "$installDir"
-Get-ChocolateyWebFile "$packageName" "$installPath" "$url" "$url64" -checksum "$checksum" -checksumType "$checksumType" -checksum64 "$checksum64" -checksumType64 "$checksumType64"
+Install-ChocolateyZipPackage "docker" "$url" "$toolsDir" "$url64" `
+ -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64
