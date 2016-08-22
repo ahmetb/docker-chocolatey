6904fac4a11791fa253c37612898630081093ddc
Stefan Scherer <scherer_stefan@icloud.com>
2016-08-22 20:41:01 +0200

    Update docker 1.12.1
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName    = 'docker'
-$url            = 'https://get.docker.com/builds/Windows/i386/docker-1.12.0.zip'
-$checksum       = '63eb40b17c88cb6d9404f5b5054e5dfc'
-$url64          = 'https://get.docker.com/builds/Windows/x86_64/docker-1.12.0.zip'
-$checksum64     = '2ccb6e53934ee08782923ea8a875be55'
+$url            = 'https://get.docker.com/builds/Windows/i386/docker-1.12.1.zip'
+$checksum       = '629df7d4874b111509acff5f3f8110ff'
+$url64          = 'https://get.docker.com/builds/Windows/x86_64/docker-1.12.1.zip'
+$checksum64     = 'ddb84b1d5c95ae587652b521c9f2ad7f'
 $checksumType   = 'md5'
 $checksumType64 = 'md5'
 $validExitCodes = @(0)
