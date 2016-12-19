e0eca64aaf4a743f74699cb9ea2a57b4066392a9
Joel Handwell <joelhandwell@gmail.com>
2016-12-19 15:46:12 -0500

    Update docker 1.12.4

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName    = 'docker'
-$url            = 'https://get.docker.com/builds/Windows/i386/docker-1.12.3.zip'
-$checksum       = '6d410819fb96563527e09ffc67427f05'
-$url64          = 'https://get.docker.com/builds/Windows/x86_64/docker-1.12.3.zip'
-$checksum64     = '26d64373f65e4ae9db71a50e8cc8b6c7'
+$url            = 'https://get.docker.com/builds/Windows/i386/docker-1.12.4.zip'
+$checksum       = '7b8100e9aa92ce4ec263224b1ac175be'
+$url64          = 'https://get.docker.com/builds/Windows/x86_64/docker-1.12.4.zip'
+$checksum64     = '6cd4fcb998b642caf170c7c6fb9037a5'
 $checksumType   = 'md5'
 $checksumType64 = 'md5'
 $validExitCodes = @(0)
