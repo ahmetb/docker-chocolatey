67bc7ab25451b3f09f172b7e3c8f91a81c2f731d
Joel Handwell <joelhandwell@gmail.com>
2016-12-21 09:07:32 -0500

    Update docker cli 1.13.0-rc3

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName    = 'docker'
-$url            = 'https://test.docker.com/builds/Windows/i386/docker-1.13.0-rc2.zip'
-$checksum       = 'd58f57f4b7e51a8300de45e96ffa9325'
-$url64          = 'https://test.docker.com/builds/Windows/x86_64/docker-1.13.0-rc2.zip'
-$checksum64     = '011cae65015eae6faf9a4264413dc867'
+$url            = 'https://test.docker.com/builds/Windows/i386/docker-1.13.0-rc3.zip'
+$checksum       = 'fb94eae850ce5a16fe302978cf853698'
+$url64          = 'https://test.docker.com/builds/Windows/x86_64/docker-1.13.0-rc3.zip'
+$checksum64     = 'f8e47abca0596d4d1a0338e67bf841b7'
 $checksumType   = 'md5'
 $checksumType64 = 'md5'
 $validExitCodes = @(0)
