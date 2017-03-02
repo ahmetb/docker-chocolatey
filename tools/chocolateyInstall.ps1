28e169681b9cc0f157f05a691dbb5eeadc15950a
Stefan Scherer <scherer_stefan@icloud.com>
2017-03-02 10:12:24 +0100

    Update Docker v17.03.0-ce

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName    = 'docker'
-$url            = 'https://get.docker.com/builds/Windows/i386/docker-1.13.1.zip'
-$checksum       = '3e31c57b0e592c5a9a489cd779c5564413c8510cfdc0b9404269623a7f6cba44'
-$url64          = 'https://get.docker.com/builds/Windows/x86_64/docker-1.13.1.zip'
-$checksum64     = '850deeac9e05a2426be335b7bf4b31757c98639eb35e0fe8d9c4b1dc8cc06c38'
+$url            = 'https://get.docker.com/builds/Windows/i386/docker-17.03.0-ce.zip'
+$checksum       = 'bc090c38554c058bd40ff25abe6e0873a16819ae3edf658d89cbc5a0768dad3f'
+$url64          = 'https://get.docker.com/builds/Windows/x86_64/docker-17.03.0-ce.zip'
+$checksum64     = '6ab9e1b7aed9ab98fe6adec6e8e45cec9f5c69e1370d7ceec5029a5df199d0e1'
 $checksumType   = 'sha256'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0)
