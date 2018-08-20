0ea4f220a380c3baee920a25b511128a8c7bc7a9
Stefan Scherer <scherer_stefan@icloud.com>
2018-08-20 08:41:44 +0200

    Download exe

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName    = 'docker'
 $url            = 'https://github.com/StefanScherer/docker-cli-builder/releases/download/18.06.0-ce/docker.exe'
-$checksum       = '8ac7d299e6bbbd4af74ac28d930875bf9e1630a6a618b29734afdaea158edea4'
+$checksum       = 'd69fac0f201b6f5727e0985990261ddc4e1cb3edd1f8db4ff46f3b2c59c3b873'
 $url64          = $url
 $checksum64     = $checksum
 $checksumType   = 'sha256'
@@ -9,5 +9,8 @@ $validExitCodes = @(0)
 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyZipPackage "docker" "$url" "$toolsDir" "$url64" `
- -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64
+$file = "$($toolsDir)\docker.exe"
+
+if (![System.IO.Directory]::Exists($toolsDir)) {[System.IO.Directory]::CreateDirectory($toolsDir)}
+
+Get-ChocolateyWebFile $packageName $file $url $url64 $checksum $checksum64 $checksumType $checksumType64
