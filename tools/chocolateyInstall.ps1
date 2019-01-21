d1e99092023869f2bc609159d51f6c43ccbc8e2a
Stefan Scherer <scherer_stefan@icloud.com>
2019-01-21 07:20:27 +0100

    Deprecate docker package

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,16 +1,1 @@
-$packageName    = 'docker'
-$url            = 'https://github.com/StefanScherer/docker-cli-builder/releases/download/18.09.0/docker.exe'
-$checksum       = '831d191ecb337889287630d4b89a24953f1416deb76f823f42f7101de23ce540'
-$url64          = $url
-$checksum64     = $checksum
-$checksumType   = 'sha256'
-$checksumType64 = 'sha256'
-$validExitCodes = @(0)
-
-$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-$file = "$($toolsDir)\docker.exe"
-
-if (![System.IO.Directory]::Exists($toolsDir)) {[System.IO.Directory]::CreateDirectory($toolsDir)}
-
-Get-ChocolateyWebFile $packageName $file $url $url64 $checksum $checksum64 $checksumType $checksumType64
+Write-Warning 'This package is superseeded by docker-cli, you should install that package from now on.'
